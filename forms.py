a87bdf4d6a086e47e6e3823d18a46944c743fb21
dhavallad <dhavallad92@gmail.com>
2017-07-03 00:27:26 -0400

    new signup feature is added.

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length 
+
+class SignupForm(Form):
+	first_name = StringField('First Name', validators=[DataRequired("Please enter your first name.")])
+	last_name = StringField('Last Name', validators=[DataRequired("Please enter your last name.")])
+	email = StringField('Email', validators=[DataRequired("Please enter your email."),Email("Please check your email address.")])
+	password = PasswordField('Password', validators=[DataRequired("Please enter your password."), Length(min=6,message="Please enter password contain 6 chracter min.")])
+	submit = SubmitField('Sign Up')
--- routes.py
+++ routes.py
@@ -1,8 +1,16 @@
 __author__ = 'Dhaval Lad'
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from models import db, User
+from forms import SignupForm
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/minimapapp'
+db.init_app(app)
+
+app.secret_key = "development-key"
+
 
 @app.route("/")
 def index():
@@ -12,5 +20,19 @@ def index():
 def about():
   return render_template("about.html")
 
+@app.route("/signup", methods =['GET', 'POST'])
+def signup():
+	form = SignupForm()
+	if request.method == "GET":
+		return render_template("signup.html", form = form)
+
+	elif request.method == "POST":
+		if form.validate() == False:
+			return render_template("signup.html", form = form)
+		else:
+			newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+			db.session.add(newuser)
+			db.session.commit()
+			return "Success!!!!!!!!"
 if __name__ == "__main__":
   app.run(debug=True)
